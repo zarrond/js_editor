5921e6f04de83919d3148e9d5754013b32d5591d
Work <TrofimovDM@nornik.ru>
2019-03-13 17:37:09 +0300

    day two

--- src/editor.js
+++ src/editor.js
@@ -10,15 +10,34 @@ String.prototype.format = function () {
                     return str;
     
                 };
-    
+
+                // Browser check
+                if ( WEBGL.isWebGLAvailable() === false ) { 
+                      
+                    document.body.appendChild( WEBGL.getWebGLErrorMessage() ); 
+                     
+                } 
+                if (window.File && window.FileReader && window.FileList && window.Blob) {
+                        // All the File APIs are supported.
+                    } else {
+                        alert('The File APIs are not fully supported in this browser.');
+                    }
+                //
+                    
+                var OPTIONS = {
+                    GEOMETRY_SIZE: 15,
+                    GRID_HELPER: true,
+                    GUI: false,
+                };
                 var container, stats;
                 var camera, scene, renderer;
                 var splineHelperObjects = [];
                 var splinePointsLength = 4;
                 var positions = [];
                 var point = new THREE.Vector3();
+                var mouse = new THREE.Vector2();
     
-                var geometry = new THREE.BoxBufferGeometry( 15, 15, 15 );
+                var geometry = new THREE.BoxBufferGeometry(OPTIONS.GEOMETRY_SIZE, OPTIONS.GEOMETRY_SIZE, OPTIONS.GEOMETRY_SIZE);
                 var transformControl;
     
                 var ARC_SEGMENTS = 200;
@@ -36,6 +55,7 @@ String.prototype.format = function () {
                 };
 
                 var infoOpened  =false;
+                var raycastingEnabled = false;
 
 
     
@@ -46,9 +66,9 @@ String.prototype.format = function () {
                     
                     
                     
-                    //document.body.appendChild(bbtn);
+
                     var menu = document.getElementById("menu");
-                    //var btn = document.createElement('btn1');
+                    
                     var btn1 = document.getElementById('btn1');
              
                     btn1.addEventListener("click",function(){
@@ -56,23 +76,26 @@ String.prototype.format = function () {
                         console.log(infoOpened);
                         menu.classList.toggle("menuMoveRight");
                     }); 
-  //menu.classList.toggle("menuMoveLeft");
+
                     
 
                     var btn2 = document.getElementById('btn2');
                     
-                            btn2.addEventListener("click",function(){
-                                
-                                document.getElementById("info1").textContent = document.getElementById("info1").textContent+"1";
-                            });
+                    btn2.addEventListener("click",function(){
+                        
+                        document.getElementById("info1").textContent = document.getElementById("info1").textContent+"1";
+                    });
                     
+
+
+                    var btn3 = document.getElementById('btn3');
+                    
+                    btn3.addEventListener("click",function(){
+                        btn3.classList.toggle("lockedButton");
+                        raycastingEnabled = !raycastingEnabled;
+                    });
                    
-                    //console.log(document.getElementById('btn1').textContent)
-                    //panel.appendChild(btn);
-                    // panel.addEventListener('hoveron',function(){
-                    //     window.alert("1");
                     
-                    // });
 
 
                     var slider = document.getElementById("range");
@@ -112,12 +135,15 @@ String.prototype.format = function () {
                     plane.position.y = - 200;
                     plane.receiveShadow = false;
                     scene.add( plane );
-    
-                    var helper = new THREE.GridHelper( 2000, 100 );
-                    helper.position.y = - 199;
-                    helper.material.opacity = 0.25;
-                    helper.material.transparent = true;
-                    scene.add( helper );
+                    
+
+                    if(OPTIONS.GRID_HELPER){
+                        var helper = new THREE.GridHelper( 2000, 100 );
+                        helper.position.y = - 199;
+                        helper.material.opacity = 0.25;
+                        helper.material.transparent = true;
+                        scene.add( helper );
+                    }
                     
     
                     // var axes = new THREE.AxesHelper( 1000 );
@@ -131,33 +157,33 @@ String.prototype.format = function () {
                     container.appendChild( renderer.domElement );
     
                     
-                    var gui = new dat.GUI();
-    
-                    gui.add( params, 'uniform' );
-                    gui.add( params, 'tension', 0, 1 ).step( 0.01 ).onChange( function ( value ) {
-    
-                        splines.uniform.tension = value;
-                        updateSplineOutline();
-    
-        } );
-                    
-                    gui.add( params, 'addPoint' );
-                    gui.add( params, 'removePoint' );
-                    gui.add( params, 'exportSpline' );
-                    gui.open();
+                    if (OPTIONS.GUI){
+                        var gui = new dat.GUI();    
+                        gui.add( params, 'uniform' );
+                        gui.add( params, 'tension', 0, 1 ).step( 0.01 ).onChange( function ( value ) {
+        
+                            splines.uniform.tension = value;
+                            updateSplineOutline();
+        
+                        } );
+                        
+                        gui.add( params, 'addPoint' );
+                        gui.add( params, 'removePoint' );
+                        gui.add( params, 'exportSpline' );
+                        gui.open();
+                    }
 
 
-                    //Popper
-
-                    var t = new Tippy();
-
-
-
-                    
-    
-                    // Controls
+        
+        
+                    // Controls 
                     var controls = new THREE.OrbitControls( camera, renderer.domElement );
                     controls.damping = 0.2;
+                    controls.addEventListener( 'mousemove', function(event){
+                        if(infoOpened){
+                            document.getElementById("info1").textContent = event.object.id+" "+event.object.position.x+" "+event.object.position.y+" "+event.object.position.z;
+                        }
+                    } ); 
                     controls.addEventListener( 'change', render );
     
                     controls.addEventListener( 'start', function () {
@@ -174,22 +200,24 @@ String.prototype.format = function () {
     
                     transformControl = new THREE.TransformControls( camera, renderer.domElement );
                     transformControl.addEventListener( 'change', render );
+                    
                     transformControl.addEventListener( 'dragging-changed', function ( event ) {
     
                         controls.enabled = ! event.value;
-    
+                        
         } );
                     scene.add( transformControl );
     
                     // Hiding transform situation is a little in a mess :()
                     transformControl.addEventListener( 'change', function () {
-    
+                        
                         cancelHideTransform();
+                        
     
                     } );
     
-                    transformControl.addEventListener( 'mouseDown', function () {
-    
+                    transformControl.addEventListener( 'mouseDown', function (event) {
+                        
                         cancelHideTransform();
     
                     } );
@@ -202,35 +230,29 @@ String.prototype.format = function () {
     
                     transformControl.addEventListener( 'objectChange', function () {
     
-                        updateSplineOutline();
+                        updateSplineOutline();                       
     
                     } );
-    
+
                     var dragcontrols = new THREE.DragControls( splineHelperObjects, camera, renderer.domElement ); //
                     dragcontrols.enabled = false;
                     dragcontrols.addEventListener( 'hoveron', function ( event ) {
-                       console.log(document.getElementById(event.object.id));
+                       //console.log(document.getElementById(event.object.id));
                        console.log(event.object);
-                       // document.activeElement.insertAdjacentText("xw");
-                        //$('#'+event.object.id).html("stringValue($(this))");
-
-                        // var tooltipSpan = document.createElement('tooltip-span');
+                       //console.log(scene.children);
+                       
                         
-                        // window.onmousemove = function (e) {
-                        //     var x = e.clientX,
-                        //         y = e.clientY;
-                        //     tooltipSpan.textContent('test');
-                        // };
-                        
-
+                        tippy("#"+event.object.uuid,
+                        {
+                          content: "content",
+                          arrow: "sharp",
+                          placement: "top"
+                  
+                        });
 
                         if(infoOpened){
                             document.getElementById("info1").textContent = event.object.id+" "+event.object.position.x+" "+event.object.position.y+" "+event.object.position.z;
                         }
-
-                        // var popup = document.createElement("popup");
-                        // popup.textContent = event.object.id;
-
 
                         transformControl.attach( event.object );
                         cancelHideTransform();
@@ -242,7 +264,9 @@ String.prototype.format = function () {
                         delayHideTransform();
     
                     } );
-    
+
+                    
+
                     var hiding;
     
                     function delayHideTransform() {
@@ -271,7 +295,7 @@ String.prototype.format = function () {
                     /*******
                      * Curves
                      *********/
-    
+                    
                     for ( var i = 0; i < splinePointsLength; i ++ ) {
     
                         addSplineObject( positions[ i ] );
@@ -315,7 +339,9 @@ String.prototype.format = function () {
                     } ) );
                     curve.mesh.castShadow = true;
                     splines.chordal = curve;
-    
+                    // splines.addEventListener("hoveron", function(event){
+                    //     document.getElementById("info1").textContent = "cown";            
+                    // })
                     for ( var k in splines ) {
     
                         var spline = splines[ k ];
@@ -327,6 +353,9 @@ String.prototype.format = function () {
                         new THREE.Vector3( - 53.56300074753207, 171.49711742836848, - 14.495472686253045 ),
                         new THREE.Vector3( - 91.40118730204415, 176.4306956436485, - 6.958271935582161 ),
                         new THREE.Vector3( - 383.785318791128, 491.1365363371675, 47.869296953772746 ) ] );
+
+
+                    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     
                 }
     
@@ -444,6 +473,24 @@ String.prototype.format = function () {
     
                 }
     
+                function onDocumentMouseMove( event ) {
+                    
+                    event.preventDefault();
+    
+                    // mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+                    // mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+                    mouse.x = event.clientX;
+                    mouse.y = event.clientY;
+
+
+                    document.getElementById("info2").textContent = "x: "+mouse.x+" y:"+mouse.y;
+    
+                }
+                    
+                    
+
+
                 function animate() {
     
                     requestAnimationFrame( animate );
@@ -454,10 +501,28 @@ String.prototype.format = function () {
                 }
     
                 function render() {
-    
+                    
+                    var raycaster = new THREE.Raycaster(); 
+                    raycaster.linePrecision = 3;
+                    raycaster.setFromCamera( mouse, camera );
+                    if(raycastingEnabled){
+                        //for(child in scene.children){
+                            var intersects = raycaster.intersectObjects(scene.children, true ); 
+                            if ( intersects.length > 0 ) {
+                                
+                                console.log( intersects[ 0 ] );
+            
+                            } else {
+                                
+                            }
+                        //}
+                    }     
                     splines.uniform.mesh.visible = params.uniform;
                     splines.centripetal.mesh.visible = params.centripetal;
                     splines.chordal.mesh.visible = params.chordal;
                     renderer.render( scene, camera );
     
                 }
+
+
+                
